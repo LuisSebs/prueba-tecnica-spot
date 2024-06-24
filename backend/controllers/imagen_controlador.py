82c81331e0a7cd12dae83f56572cf145f39ddcc4
LuisSebs <sebastian_luis@ciencias.unam.mx>
2024-06-23 20:50:38 -0600

    feat: endpoints imagenes implementados

--- backend/controllers/imagen_controlador.py
+++ backend/controllers/imagen_controlador.py
@@ -2,7 +2,7 @@ import io
 from flask import Blueprint, jsonify
 from models import model_imagen
 from base64 import encodebytes
-
+from PIL import Image
 
 imagen = Blueprint('imagen', __name__)
 
@@ -11,35 +11,37 @@ def get_imagenes():
     # Imagenes
     imagenes = model_imagen.get_imagenes()
     # Imagenes como diccionario
-    imagenes_json = []
+    imagenes_dic = []
     for i in imagenes:
-        with open(i.ruta, 'rb') as f:
-            img = f.read()
-            img_base64 = base64.b64encode(img).decode('utf-8')
-        imagenes_json.append({
+        pil_img = Image.open("imagenes_productos/"+i.ruta, mode='r')
+        byte_arr = io.BytesIO()
+        pil_img.save(byte_arr, format='JPEG')
+        encoded_img = encodebytes(byte_arr.getvalue()).decode('ascii')
+        imagenes_dic.append({
             "idImagen": i.idImagen,
             "idAutomovil": i.idAutomovil,
-            "imagen": img_base64
+            "imagen": encoded_img
         })
     # JSON
-    return jsonify({"data": imagenes})
+    return jsonify({"data": imagenes_dic})
 
 @imagen.route('/get_imagenes_by_idAutomovil/<idAutomovil>', methods=['POST','GET'])
 def get_imagenes_by_idAutomovil(idAutomovil):
     # Imagenes
     imagenes = model_imagen.get_imagenes_by_idAutomovil(idAutomovil=idAutomovil)
     # Imagenes como diccionario
-    imagenes_json = []
+    imagenes_dic = []
     for i in imagenes:
-        with open("imagenes_productos/"+i.ruta, 'rb') as f:
-            img = f.read()
-            img_base64 = base64.b64encode(img).decode('utf-8')
-        imagenes_json.append({
+        pil_img = Image.open("imagenes_productos/"+i.ruta, mode='r')
+        byte_arr = io.BytesIO()
+        pil_img.save(byte_arr, format='JPEG')
+        encoded_img = encodebytes(byte_arr.getvalue()).decode('ascii')
+        imagenes_dic.append({
             "idImagen": i.idImagen,
             "idAutomovil": i.idAutomovil,
-            "imagen": img_base64
+            "imagen": encoded_img
         })
     # JSON
-    return jsonify({"data": imagenes})
+    return jsonify({"data": imagenes_dic})
 
 
