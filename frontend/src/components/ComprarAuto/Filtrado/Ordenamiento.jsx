7c5de9fbe9748152541f61b928448af3fcc4cde2
LuisSebs <sebastian_luis@ciencias.unam.mx>
2024-06-25 14:47:23 -0600

    update: Ordenamiento estilos

--- frontend/src/components/ComprarAuto/Filtrado/Ordenamiento.jsx
+++ frontend/src/components/ComprarAuto/Filtrado/Ordenamiento.jsx
@@ -2,84 +2,42 @@ import React from 'react'
 import { CaretDown, CaretUp } from '@phosphor-icons/react/dist/ssr'
 import { useState } from 'react'
 import { mergeSort } from '../../../utils/mergeSort'
+import { useRef } from 'react'
 
 export const Ordenamiento = ({ autosState, autosStateAux, setAutosStateAux }) => {
-
     /**Estados de interface */
     const [textOrdenarPorButton, setTextOrdenarPorButton] = useState('Ordenar Por')
     const [isOrdenarPorVisible, setIsOrdenarPorVisible] = useState(false)
-    const [isBaratoSelected, setIsBaratoSelected] = useState(false)
-    const [isCaroSelected, setIsCaroSelected] = useState(false)
-    const [isNovedadesSelected, setIsNovedadesSelected] = useState(false)
-    const [isAntiguedadesSelected, setIsAntiguedadesSelected] = useState(false)
+
+    const ordenarPorAux = (by) => {
+        setIsOrdenarPorVisible(false)
+        const newAutos = [...autosStateAux]
+        mergeSort(newAutos,by)
+        setAutosStateAux(newAutos)
+    }
 
     const viewOrdenarPor = (event) => {
         setIsOrdenarPorVisible(!isOrdenarPorVisible)
     }
 
     const ordenarPorMasBarato = (event) => {
-
-        const newAutos = [...autosStateAux]
-        mergeSort(newAutos,'precioBarato')
-        setAutosStateAux(newAutos)
-
-        setIsBaratoSelected(true)
-        setIsOrdenarPorVisible(false)
-
-        setIsCaroSelected(false)
-        setIsNovedadesSelected(false)
-        setIsAntiguedadesSelected(false)
+        ordenarPorAux('precioBarato')
         setTextOrdenarPorButton('Mas barato')
-        
     }
 
     const ordenarPorMasCaro = (event) => {
-
-        const newAutos = [...autosStateAux]
-        mergeSort(newAutos,'precioCaro')
-        setAutosStateAux(newAutos)
-
-        setIsCaroSelected(true)
-        setIsOrdenarPorVisible(false)
-
-        setIsBaratoSelected(false)
-        setIsNovedadesSelected(false)
-        setIsAntiguedadesSelected(false)
+        ordenarPorAux('precioCaro')
         setTextOrdenarPorButton('Mas caro')
-        
     }
 
     const ordenarPorNovedades = (event) => {
-
-        const newAutos = [...autosStateAux]
-        mergeSort(newAutos,'fechaEntradaNuevo')
-        setAutosStateAux(newAutos)
-
-        setIsNovedadesSelected(true)
-        setIsOrdenarPorVisible(false)
-
-
-        setIsBaratoSelected(false)
-        setIsCaroSelected(false)
-        setIsAntiguedadesSelected(false)
+        ordenarPorAux('fechaEntradaNuevo')
         setTextOrdenarPorButton('Novedades')
-
     }
 
     const ordenarPorAntiguedades = (event) => {
-
-        const newAutos = [...autosStateAux]
-        mergeSort(newAutos,'fechaEntradaViejo')
-        setAutosStateAux(newAutos)
-
-        setIsAntiguedadesSelected(true)
-        setIsOrdenarPorVisible(false)
-
-        setIsCaroSelected(false)
-        setIsBaratoSelected(false)
-        setIsNovedadesSelected(false)
+        ordenarPorAux('fechaEntradaViejo')
         setTextOrdenarPorButton('Antiguedades')
-
     }    
 
   return (
@@ -92,10 +50,10 @@ export const Ordenamiento = ({ autosState, autosStateAux, setAutosStateAux }) =>
             </button>
             {isOrdenarPorVisible && 
             <div className='dropdown-ordenar-por'>
-                <button className={`${isCaroSelected ? 'selected' : ''}`} onClick={e => ordenarPorMasCaro(e)}>Mas caro</button>
-                <button className={`${isBaratoSelected ? 'selected' : ''}`} onClick={e => ordenarPorMasBarato(e)}>Mas barato</button>                
-                <button className={`${isNovedadesSelected ? 'selected' : ''}`} onClick={e => ordenarPorNovedades(e)}>Novedades</button>
-                <button className={`${isAntiguedadesSelected ? 'selected' : ''}`} onClick={e => ordenarPorAntiguedades(e)}>Antiguedades</button>
+                <button onClick={e => ordenarPorMasCaro(e)}>Mas caro</button>
+                <button onClick={e => ordenarPorMasBarato(e)}>Mas barato</button>                
+                <button onClick={e => ordenarPorNovedades(e)}>Novedades</button>
+                <button onClick={e => ordenarPorAntiguedades(e)}>Antiguedades</button>
             </div>
             }
         </div>
